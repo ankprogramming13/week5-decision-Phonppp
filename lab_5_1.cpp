ed2f7eb327cafbab952d1b9df75b69b5c6728602
Unknown <kwangkg.30@gmail.com>
2017-12-15 13:35:49 +0700

    ข้อ 5_2

--- lab_5_1.cpp
+++ lab_5_1.cpp
@@ -17,8 +17,11 @@ int main ()
     cin >> name2;
 
     // เขียนเงื่อนไขให้ถูกต้องซะ หนูๆ
-    cout << "The two names are equal" << endl;
+    if cout << "The two names are equal" << endl;
+    else {
     cout << "The two names are not equal" << endl;
+    }
+
 
     return 0;
 }
